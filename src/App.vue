dba98af973833576b9f31676c42a041f8fa4f7a6
Hoan Nguyễn Đình <ndhoanit1112@gmail.com>
2021-07-19 23:23:05 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <Question />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+import Question from './components/Question.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Question
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      total: 0,
+    }
+  },
+  mounted: function(){
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', { method: 'get' })
+      .then(res => res.json())
+      .then(jsonData => {
+        this.questions = jsonData.results;
+      });
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    Header
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
